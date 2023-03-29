ec1250a8b763c53e32ffbe4fb623c785fcd3bcd0
rishisankhla <57127587+rishisankhla@users.noreply.github.com>
2023-03-29 03:03:56 +0100

    final_coding_part_3
    final_coding_part_3

--- drawing 1/sketch.js
+++ drawing 1/sketch.js
@@ -17,6 +17,10 @@
 //var draw_art;
 //function art_4
 
+//function art_5
+//var color_circle;
+//function art_5
+
 function setup() {
   createCanvas(800, 800);
 //  pixelDensity(1);
@@ -44,6 +48,10 @@ function setup() {
 //    draw_art = new art_4();
 //    draw_art.setup();
     //function art_4
+    
+    //function art_5
+//    color_circle = new art_5();
+    //function art_5
 }
 
 function draw() {
@@ -75,6 +83,11 @@ function draw() {
     //function art_4
 //    draw_art.draw(mouseX,mouseY); //don't need any background with this
     //function art_4
+    
+    //function art_5
+//    color_circle.draw();
+    //function art_5
+
 }
 
 function art_1(){
@@ -240,9 +253,81 @@ function art_4(){
     }
 }
 
+function art_5(){
+    
+    this.size_c = 10;
+    this.color_trip = 3;
+    this.color_trip_array = [];
+    this.num_c = round(width/this.size_c)*round(height/this.size_c);
+    
+    function single_circle(x,y,size_c,fill_c){
+        push();
+        noStroke();
+        fill(fill_c);
+        ellipse(x,y,size_c,size_c);
+        pop();
+    }
+    
+    this.draw = function(){
+        
+        var c_array = [];
+        for(var w=1;w<=this.num_c;w++){
+            c_array.push(new art_5_mini());
+        }
+        for(var s=0;s<c_array.length;s++){
+            c_array[s].select_number(s+1);
+        }
+        var c=0;
+        for(var i=0;i<round(width/this.size_c);i++){
+            
+            var x = this.size_c*(i%round(width/this.size_c));
+            var m1 = map(i,0,round(width/this.size_c),0,255);
+            
+            for(var p=0;p<round(height/this.size_c);p++){
+                
+                var y = this.size_c*(p%round(height/this.size_c));
+                var m2 = map(p,0,round(height/this.size_c),0,255);
+                var single_element = c_array[c];
+                single_element.x=x;
+                single_element.y=y;
+                var d =single_element.cal_dis(mouseX,mouseY);
+                var cal_di = sqrt(sq(width)+sq(height));
+                var m3 = map(d,0,cal_di,0,255);
+                this.color_trip_array = [];
+                this.color_trip_array.push([m1,m2,m3]);
+                this.color_trip_array.push([m1,m3,m2]);
+                this.color_trip_array.push([m2,m3,m1]);
+                this.color_trip_array.push([m2,m1,m3]);
+                this.color_trip_array.push([m3,m2,m1]);
+                this.color_trip_array.push([m3,m1,m2]);
+                single_element.single_circle(this.size_c,
+                                this.color_trip_array[this.color_trip]);
+                c=c+1;
+            }
+        }     
+    }
+}
 
-
-
+function art_5_mini(){
+    this.circle_number = 0;
+    this.x=0;
+    this.y=0;
+    this.select_number = function(a){
+        this.circle_number = a;
+    }
+    this.single_circle = function(size_c,fill_c){
+        
+        push();
+        noStroke();
+        fill(fill_c);
+        ellipse(this.x,this.y,size_c,size_c);
+        pop();
+    }
+    this.cal_dis = function(m_x,m_y){
+        var d =dist(this.x,this.y,m_x,m_y);
+        return d;
+    }
+}
 
 
 
